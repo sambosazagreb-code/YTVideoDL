d4d9b294a73841326c159b5cd93fdd164c87b808
sambosazagreb-code <sambosazagreb@gmail.com>
2026-09-13 09:32:34 -0700

    Update startserver.py

--- startserver.py
+++ startserver.py
@@ -1,19 +1,8 @@
 import subprocess
-import time
-from pathlib import Path
-APP_DIR = Path(__file__).parent
 
 def main():
-    
-    print("Starting Node.js server...")
-    subprocess.Popen(['node', str(APP_DIR / 'app.js')], shell=True)
-    
-    
-    time.sleep(2)
-    
-    
-   print("Opening browser at http://localhost:3000...")
-    subprocess.Popen(['start', 'http://localhost:3000'], shell=True)
+    print("Running node app.js...")
+    subprocess.run(["node", "app.js"], shell=True)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
